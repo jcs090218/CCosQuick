8d256f8a18b51d957103cb202cfa8c0288f55b32
Jen-Chieh Shen <jcs090218@gmail.com>
2019-12-23 17:00:41 +0800

    Add core.

---
+++ assets/scripts/CQ_RotateAction.ts
@@ -0,0 +1,31 @@
+/**
+ * $File: CQ_RotateAction.ts $
+ * $Date: 2019-12-23 16:59:46 $
+ * $Revision: $
+ * $Creator: Jen-Chieh Shen $
+ * $Notice: See LICENSE.txt for modification and distribution information
+ *                   Copyright © 2019 by Shen, Jen-Chieh $
+ */
+
+
+const {ccclass, property} = cc._decorator;
+
+@ccclass
+export default class NewClass extends cc.Component {
+
+    @property(cc.Label)
+    label: cc.Label = null;
+
+    @property
+    text: string = 'hello';
+
+    // LIFE-CYCLE CALLBACKS:
+
+    // onLoad () {}
+
+    start () {
+
+    }
+
+    // update (dt) {}
+}
