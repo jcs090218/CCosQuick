05dec6d15546a013d5fffd2405c917856fd6e338
Jen-Chieh Shen <jcs090218@gmail.com>
2019-12-27 18:50:46 +0800

    Revert ts way of tweening.

--- assets/scripts/__test/CQ_TestNode.ts
+++ assets/scripts/__test/CQ_TestNode.ts
@@ -53,6 +53,12 @@ export default class CQ_TestNode extends cc.Component {
         }
 
         if (CQ_Input.getKey(CQ_KeyCode.Q)) {
+            // let newPos : cc.Vec2 = this._startingPoint;
+            // newPos.x += 10;
+            // let mto = cc.moveTo(1, cc.p(newPos.x, newPos.y)).easing(cc.easeInOut(1));
+            // this.node.stopAllActions();
+            // this.node.runAction(mto);
+
             cc.tween(this.node).to(1, {
                 scale: 2,
                 position: cc.v2(100, 100)
@@ -60,6 +66,12 @@ export default class CQ_TestNode extends cc.Component {
         }
 
         if (CQ_Input.getKey(CQ_KeyCode.W)) {
+            // let newPos : cc.Vec2 = this._startingPoint;
+            // newPos.x -= 10;
+            // let mto = cc.moveTo(1, cc.p(newPos.x, newPos.y)).easing(cc.easeInOut(1));
+            // this.node.stopAllActions();
+            // this.node.runAction(mto);
+
             cc.tween(this.node).to(1, {
                 scale: 1,
                 position: this._startingPoint
