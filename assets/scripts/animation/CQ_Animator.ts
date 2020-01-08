44a8b6ae3df22210d3ddc914a902933330cbfdd9
Jen-Chieh Shen <jcs090218@gmail.com>
2020-01-08 15:32:28 +0800

    First add animator.

---
+++ assets/scripts/animation/CQ_Animator.ts
@@ -0,0 +1,19 @@
+/**
+ * $File: CQ_Animator.ts $
+ * $Date: 2020-01-08 15:28:34 $
+ * $Revision: $
+ * $Creator: Jen-Chieh Shen $
+ * $Notice: See LICENSE.txt for modification and distribution information
+ *                   Copyright © 2020 by Shen, Jen-Chieh $
+ */
+const { ccclass, property, requireComponent } = cc._decorator;
+
+@ccclass
+export default class CQ_Animator extends cc.Component {
+    /* Variables */
+
+    /* Setter & Getter */
+
+    /* Functions */
+
+}
